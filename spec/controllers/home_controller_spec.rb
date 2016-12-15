b663ae830aa2bba1e88c4b0c6795b1ac55e42d43
Kalebe Alves <kalebeapsoares@gmail.com>
2016-12-15 14:41:43 -0200

    Home controller spec refactored

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -4,11 +4,12 @@ RSpec.describe HomeController, type: :controller do
   describe "GET index" do
     let (:manufacturers_names) { WebMotorsRequestAPI.get_makes_names }
 
-    it "stores manufacturers from WebMotors at Make model" do
+    before(:each) do
       get :index
-      makes_names = assigns[:makes].map { |make| make.name }.uniq
-      expect(response).to have_http_status :ok
-      expect(makes_names).to match_array (manufacturers_names)
+      @makes_names = assigns[:makes].map { |make| make.name }.uniq
     end
+
+    it { expect(response).to have_http_status :ok }
+    it { expect(@makes_names).to match_array (manufacturers_names) }
   end
 end

1718cda4037c26d191e88d3cf5be1451bb6b9d2b
Kalebe Alves <kalebeapsoares@gmail.com>
2016-12-15 14:21:36 -0200

    Spec for Model controller

--- app/controllers/models_controller.rb
+++ app/controllers/models_controller.rb
@@ -1,21 +1,7 @@
 class ModelsController < ApplicationController
   def index
-    #search the models
-    uri = URI("http://www.webmotors.com.br/carro/modelos")
-
-    # Make request for Webmotors site
-    make = Make.where(webmotors_id: params[:webmotors_make_id])[0]
-
-    response = Net::HTTP.post_form(uri, { marca: params[:webmotors_make_id] })
-    models_json = JSON.parse response.body
-
-    # debugger
-
-    # Itera no resultado e grava os modelos que ainda não estão persistidas
-    models_json.each do |json|
-      if Model.where(name: json["Nome"], make_id: make.id).size == 0
-        Model.create(make_id: make.id, name: json["Nome"])
-      end
-    end
+    make = Make.find_by webmotors_id: params[:webmotors_make_id]
+    make.populate_models_from_webmotors
+    @models = make.models
   end
 end
---
+++ spec/controllers/models_controller_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe ModelsController, type: :controller do
+  describe "GET index" do
+    before(:each) do
+      Make.populate_from_webmotors
+      @make = Make.all.sample
+      get :index, webmotors_make_id: @make.webmotors_id
+    end
+
+    it { expect(response).to have_http_status :ok }
+    it { expect(assigns["models"].sample.make_id).to eq(@make.id)}
+  end
+end

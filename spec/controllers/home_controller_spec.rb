bbbf4ab4a1e7e23c48bcc14d8d10609e9332f03a
Kalebe Alves <kalebeapsoares@gmail.com>
2016-12-16 14:21:05 -0200

    Refactoring specs for home controller

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,14 +2,12 @@ require 'rails_helper'
 
 RSpec.describe HomeController, type: :controller do
   describe "GET index" do
-    let (:manufacturers_names) { WebMotorsRequestAPI.get_makes_names }
-
     before(:each) do
       get :index
-      @makes_names = assigns[:makes].map { |make| make.name }.uniq
     end
 
     it { expect(response).to have_http_status :ok }
-    it { expect(@makes_names).to match_array (manufacturers_names) }
+    it { expect(response).to render_template :index }
+    it { expect(assigns[:makes]).to eq(Make.all) }
   end
 end
